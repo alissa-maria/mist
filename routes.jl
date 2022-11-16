8786888d391dff6bc48b50c3ff3de7fe01dd9ec0
alissakruiniger <62930577+alissakruiniger@users.noreply.github.com>
2022-11-16 16:55:03 +0100

    Gallery (needs some fixing)

--- routes.jl
+++ routes.jl
@@ -2,6 +2,9 @@ using Genie.Router, Motregen.HomeController, Motregen.CommentsController
 
 route("/", HomeController.index)
 
-route("/create_comment", CommentsController.create, method = POST, named = :create_comment)
+route("/gallery") do 
+    serve_static_file("gallery.html")
+end
 
 route("/guestbook", CommentsController.guestbook)
+route("/create_comment", CommentsController.create, method = POST, named = :create_comment)
