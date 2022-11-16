63ac0e2a2419d14b6045cf0a134b0e993ba4a400
alissakruiniger <62930577+alissakruiniger@users.noreply.github.com>
2022-11-16 09:31:52 +0100

    Get comments

--- app/resources/comments/CommentsController.jl
+++ app/resources/comments/CommentsController.jl
@@ -1,18 +1,15 @@
 module CommentsController
 
-using Genie, Genie.Renderer, Genie.Requests, SearchLight, Markdown
-using Motregen.Comments
+using Genie.Renderer.Html, SearchLight, Motregen.Comments
+# using Markdown
 
-function index()
-  html(:home, :index, title = "home")
-  html(:comments, :guestbook, comments = all(Comment), layout = :empty)
-  # html(:home, :index, comments = all(Comment, order = SQLOrder(:id, "DESC")))
-  # html(:home, :index, comments = all(Comment, SQLQuery(where = [SQLWhereExpression("page = '/'")], order = SQLOrder(:created_at, "DESC"))), title = "home")
+function guestbook()
+  html(:comments, :guestbook, comments = all(Comment), layout = :guestbook)
 end
 
-function create()
-  sanitized_text = Markdown.htmlesc(postpayload(:text)) |> strip
-  Comment(name = postpayload(:name), text = sanitized_text, page = postpayload(:page)) |> save && redirect("/guestbook") 
-end
+# function create()
+#   sanitized_text = Markdown.htmlesc(postpayload(:text)) |> strip
+#   Comment(name = postpayload(:name), text = sanitized_text, page = postpayload(:page)) |> save && redirect("/guestbook") 
+# end
 
 end
--- routes.jl
+++ routes.jl
@@ -1,3 +1,7 @@
-using Genie.Router, Motregen.HomeController
+using Genie.Router, Motregen.HomeController, Motregen.CommentsController
 
 route("/", HomeController.index)
+
+# route("/create_comment", CommentsController.create, method = POST, named = :create_comment)
+
+route("/guestbook", CommentsController.guestbook)
