1084f1bcafc860c90605aabf08fb1eea04359be1
alissakruiniger <62930577+alissakruiniger@users.noreply.github.com>
2022-11-24 22:49:23 +0100

    Epic dark mode

--- public/js/guestbook.js
+++ public/js/guestbook.js
@@ -1,12 +1,12 @@
 var style =
-  "body{background-color:#2c2c2c;color:#fff8d1;}#header-box{padding:0;margin-top:28px;}.comment{background-color:#3c3c3c;box-shadow:3px 3px #000;}";
+  "body{background-color:#2c2c2c;color:#fff8d1;}#header-box{padding:0;margin-top:28px;}.comment{background-color:#3c3c3c;box-shadow:3px 3px #000;}a:visited{color:lightblue;}";
 
 var stylesheet = document.createElement("style");
 stylesheet.innerText = style;
 
 var metatag = document.createElement("meta");
 metatag.name = "color-scheme";
-metatag.content = "dark light"; 
+metatag.content = "dark";
 
 var hour = new Date().getHours();
 
