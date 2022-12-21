124571878f8e0b1e17e099405ffffcd182ef62ea
alissakruiniger <62930577+alissakruiniger@users.noreply.github.com>
2022-12-21 10:12:20 +0100

    Capital letters

--- app/resources/home/HomeController.jl
+++ app/resources/home/HomeController.jl
@@ -3,15 +3,15 @@ module HomeController
 using Genie.Renderer.Html, SearchLight
 
 function index()
-  html(:home, :index, title = "home")
+  html(:home, :index, title = "Home")
 end
 
 function contact()
-  html(:home, :contact, title = "contact")
+  html(:home, :contact, title = "Contact")
 end
 
 function nse()
-  html(:home, :nse, title = "not software engineering")
+  html(:home, :nse, title = "Not Software Engineering")
 end
 
 end
