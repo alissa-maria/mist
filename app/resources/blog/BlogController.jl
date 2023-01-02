62ec930494cb09eb926b7a3d1c5d7d47d62ef2cd
alissakruiniger <62930577+alissakruiniger@users.noreply.github.com>
2023-01-02 20:57:17 +0100

    It works (but at what cost)

--- app/resources/blog/BlogController.jl
+++ app/resources/blog/BlogController.jl
@@ -1,6 +1,6 @@
 module BlogController
 
-using Genie, Genie.Renderer.Html, Dates, YAML
+using Genie.Renderer.Html, Dates, YAML
 
 struct Post
   title::String
@@ -15,9 +15,9 @@ Posts = Vector{Post}()
 
 function initialize()
   if (isempty(Posts))
-    post_names = readdir("app/resources/blog/posts")
-    for_each(post_names) do post_name
-      data = YAML.load_file("app/resources/blog/posts/" * post_name)
+    post_names = readdir("app/resources/blog/views/posts")
+    foreach(post_names) do post_name
+      data = YAML.load_file("app/resources/blog/views/posts/" * post_name)
       tags = split(data["tags"], ", ")
       push!(Posts, Post(data["title"], data["description"], tags, data["date"], data["link"], post_name))
     end
@@ -33,7 +33,7 @@ function blogpost(link::SubString{String})
   pst_index = findfirst(x -> x.link == link, Posts)
   if (!isnothing(pst_index))
     pst = getindex(Posts, pst_index)
-    html(:blog, pst.filename, title = pst.title)
+    html(:blog, "posts/" * pst.filename, layout = :post)
   end
 end
 
