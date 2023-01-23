dedc9d1f7beac767527f6445cafcfd42e1d06752
alissakruiniger <62930577+alissakruiniger@users.noreply.github.com>
2023-01-23 11:23:42 +0100

    Refactored create comment func, still needs work

--- app/resources/comments/CommentsController.jl
+++ app/resources/comments/CommentsController.jl
@@ -4,12 +4,22 @@ using Genie.Renderer.Html, Genie.Requests, SearchLight, Motregen.Comments
 import Markdown
 
 function guestbook()
-  html(:comments, :guestbook, comments = all(Comment, order = SQLOrder(:id, "DESC")), layout = :guestbook)
+  html(:comments, :guestbook, comments=all(Comment, order=SQLOrder(:id, "DESC")), layout=:guestbook)
 end
 
 function create()
-  sanitized_text = Markdown.htmlesc(postpayload(:text)) |> strip
-  Comment(name = postpayload(:name), text = sanitized_text, page = postpayload(:page)) |> save && redirect("/guestbook") 
+  sanitized_name::String = sanitize(postpayload(:name))
+  sanitized_text::String = sanitize(postpayload(:text))
+  sanitized_page::String = sanitize(postpayload(:page))
+
+  with(Comment(name=sanitized_name, text=sanitized_text, page=sanitized_page)) do comment
+    save(comment)
+    redirect("/guestbook", status_code=302) # route should not be hardcoded
+  end
+end
+
+function sanitize(text::String)
+  Markdown.htmlesc(text) |> strip
 end
 
 end
